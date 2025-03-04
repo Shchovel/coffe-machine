750a317ae0118d62b96324d18883072e14826725
school <school@A45PC12>
2025-03-04 09:03:41 +0200

    Add project files.

---
+++ Coffe-machine/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Coffe_machine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
