24894807bace3565825f64b5a737e431c7fa2296
school <school@A45PC12>
2025-03-11 09:51:15 +0200

    add classes

--- Coffe-machine/Program.cs
+++ Coffe-machine/Program.cs
@@ -20,7 +20,27 @@ Console.WriteLine("\t Menu:");
 Console.WriteLine("- Admin");
 Console.WriteLine("\t1. Add water");
 Console.WriteLine("\t2. Add coffe");
+Console.WriteLine("\t3. Add sugar");
+Console.WriteLine("\t4. Add milk");
+Console.WriteLine("\t5. show statistik");
+Console.WriteLine("\t6. take cash");
 
 Console.WriteLine("- User");
 Console.WriteLine("\t1. Order");
 
+
+public class Drink
+{
+    public string Name { get; set; }
+    public int Size { get; set; }
+    public double Price { get; set; }
+    public int RadiationLevel { get; set; }
+    public string Topings { get; set; }
+}
+
+public class Ingradient
+{
+    public string Name { get; set; }
+    public string Weight { get; set; }
+   
+}
