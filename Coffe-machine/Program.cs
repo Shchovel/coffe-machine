f4def4966cd1069a9191798f12400582b20376b2
school <school@A45PC12>
2025-03-18 09:57:09 +0200

    pominav

--- Coffe-machine/Program.cs
+++ Coffe-machine/Program.cs
@@ -18,12 +18,49 @@ Console.WriteLine("-----CoffeMachine-----");
 Console.WriteLine("\t Menu:");
 
 Console.WriteLine("- Admin");
-Console.WriteLine("\t1. Add water");
-Console.WriteLine("\t2. Add coffe");
-Console.WriteLine("\t3. Add sugar");
-Console.WriteLine("\t4. Add milk");
+Console.WriteLine("\t1. Add ingredient");
+
 Console.WriteLine("\t5. show statistik");
 Console.WriteLine("\t6. take cash");
+Console.WriteLine("\t7. Add drink");
+
+Drink item = new();
+
+while (true)
+{
+    Console.Write("Your choice: ");
+    int choice = Convert.ToInt32(Console.ReadLine());
+
+    switch (choice)
+    {
+        case 1:
+            Console.Write("Enter product Name: ");
+            item.Name = Console.ReadLine();
+            Console.Write("Enter product Weight: ");
+            item.Weight = Console.ReadLine();
+            break;
+        case 7:
+            Console.Write("Enter product Name: ");
+            item.Name = Console.ReadLine();
+            Console.Write("Enter product Size: ");
+            item.Size = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter product Price: ");
+            item.Price = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter product RadiationLevel: ");
+            item.RadiationLevel = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter product Topings: ");
+            item.Topings = Console.ReadLine();
+            break;
+        case 4:
+            Console.WriteLine("------- Drink ---------");
+            Console.WriteLine($"Name: {item.Name}");
+            Console.WriteLine($"Size: {item.Size}");
+            Console.WriteLine($"Price: {item.Price}");
+            Console.WriteLine($"RadiationLevel: {item.RadiationLevel}");
+            Console.WriteLine($"Topings: {item.Topings}");
+            break;
+    }
+}
 
 Console.WriteLine("- User");
 Console.WriteLine("\t1. Order");
